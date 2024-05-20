72f20da0c02a1902a59256719d06f47ba39ff11a
NitishGhormade <118716634+NitishGhormade@users.noreply.github.com>
2024-05-20 16:17:33 +0530

    index.js

--- URL_Shortner/index.js
+++ URL_Shortner/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const sid = require("shortid")
+const path = require("path")
 
 // Connection 
 mongoose
@@ -17,6 +18,9 @@ const urlSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+    noClicks: {
+        type: Number
     }
 })    
 const URLmodel = mongoose.model("url", urlSchema)
@@ -24,36 +28,54 @@ const URLmodel = mongoose.model("url", urlSchema)
 // Express
 const app = express()
 
+
+// Set view engine of express
+app.set("views", path.join(__dirname, "views"))
+app.set('view engine', 'ejs')
+
+
 // Middlewares
-app.use(express.json())
+app.use(express.json()) // To Support JSON Data
+app.use(express.urlencoded({ extended: false })) // To Support FORM Data
 
 // Routes
+app.get("/url", async (req, res) => {
+    const allUrl = await URLmodel.find()
+    
+    return res.render("home", {allUrl: allUrl})
+})
+
 app.post("/url", async (req, res) => {
     const body = req.body;
     if(!body){
         return res.status(400).json({"status": "Enter the URL"})
     }
-
     const checkURL = await URLmodel.findOne({originalURL: body.url})
     if(checkURL){
-       return res.json({"status": `URL Already Exists having Code: ${checkURL.shortURLcode}`}) 
+       return res.render("home", {"shortURLcode": checkURL.shortURLcode}) 
     }
 
     const shortID = sid.generate()
     await URLmodel.create({
         originalURL: body.url,
-        shortURLcode: shortID
+        shortURLcode: shortID,
+        noClicks: 0
     })
 
-    return res.json({"status": `Short URL Code: ${shortID}`})
+    return res.render("home", {"shortURLcode": shortID})
 })
 
-app.get("/:shortID", async (req, res) => {
+app.get("/url/:shortID", async (req, res) => {
     const shortID = req.params.shortID;
     const urlObj = await URLmodel.findOne({ shortURLcode: shortID})
+
+    if(!urlObj){
+        return res.json({"status": "No such URL Exists"})
+    }
+
+    
 
     res.redirect(urlObj.originalURL)
 })
 
-
-app.listen(8080, () => console.log("Server Started!"))
+app.listen(8080, () => console.log("Server Started at PORT 8080!"))
